36c4b98cd9db3fc3cf72f5b913830b94e0ea00c0
Official3173 <42709932+Official3173@users.noreply.github.com>
2018-09-11 17:48:47 +1000

    Added imageOverlay folder
    Moved imageOverlay.py into a class with a single method, added tester python file, and included correct fonts.

---
+++ imageOverlay/imageOverlay.py
@@ -0,0 +1,89 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+class ImageOverlay():
+    
+    def __init__(self, star_rating, model_num, kwh):
+        self.star_rating = star_rating
+        self.model_num = model_num
+        self.kwh = kwh
+        self.final_img_id = None
+
+    def generate_image(self):
+        '''
+        Opens the correct blank template image.
+        '''
+
+        if self.star_rating == '0.5':
+            self.image_template_id = '0.5 Stars Final Template'
+        
+        elif self.star_rating == '1':
+            self.image_template_id = '1 Stars Final Template'
+        
+        elif self.star_rating == '1.5':
+            self.image_template_id = '1.5 Stars Final Template'
+
+        elif self.star_rating == '2':
+            self.image_template_id = '2 Stars Final Template'
+        
+        elif self.star_rating == '2.5':
+            self.image_template_id = '2.5 Stars Final Template'
+        
+        elif self.star_rating == '3':
+            self.image_template_id = '3 Stars Final Template'
+        
+        elif self.star_rating == '3.5':
+            self.image_template_id = '3.5 Stars Final Template'
+        
+        elif self.star_rating == '4':
+            self.image_template_id = '4 Stars Final Template'
+
+        elif self.star_rating == '4.5':
+            self.image_template_id = '4.5 Stars Final Template'
+
+        elif self.star_rating == '5':
+            self.image_template_id = '5 Stars Final Template'
+
+        # Opens the image from the correct directory
+        img = Image.open('/home/mitchell/Desktop/energy-site-master/mywebsite/images/static/images/Dishwasher/' + self.image_template_id + '.png')
+
+        #aragon sans bold
+        #replaces "<Clothes Dryer Model>" & "<Load capacity xx kg"
+        fnt1 = ImageFont.truetype('Aragon_Sans_Bold.otf', 42)
+
+        #this font looks incorrect
+        #replaces "xxx"
+        fnt3 = ImageFont.truetype('OpenSans-Regular.ttf', 105)
+
+        #replaces "<program name> used once per week"
+        fnt4 = ImageFont.truetype('Aragon_Sans_Bold.otf', 18)
+
+        d1 = ImageDraw.Draw(img)
+        d2 = ImageDraw.Draw(img)
+        d3 = ImageDraw.Draw(img)
+        d4 = ImageDraw.Draw(img)
+
+        d3_text_size = d3.textsize(self.kwh, font=fnt3)
+        d3_text_midpoint = d3_text_size[0] / 2
+
+        d1_text_size = d1.textsize(self.model_num, font=fnt1 )
+        d1_text_midpoint = d1_text_size[0] / 2
+
+        #COLORS: ORANGE fill = (241, 92, 48); WHITE fill = (255, 255, 255)
+
+        d1.text((538 - d1_text_midpoint , 1340), self.model_num,        font = fnt1, fill=(241, 92, 48))
+        #d2.text((538, 1600), 'big ol pp capacity 25kg',  font = fnt1, fill=(241, 92, 48) )
+        d3.text((538 - d3_text_midpoint, 1120), self.kwh,               font = fnt3, fill=(255, 255, 255))
+
+        # Unique id to be stored for each image
+        self.final_img_id = 'img123'
+
+        #saves image to filepath, with unique id as image name
+        img.save('/home/mitchell/Desktop/temporaru/' + self.final_img_id + '.png')
+
+
+'''
+Initializes the object, and generates an image with input from parameters
+'''
+#image = ImageOverlay('1.5', 'Clothes Dryer 3000, for big loads', '366')
+#image.generate_image()
---
+++ imageOverlay/image_tester.py
@@ -0,0 +1,4 @@
+from imageOverlay import ImageOverlay
+
+image = ImageOverlay('0.5','12','15')
+image.generate_image()
