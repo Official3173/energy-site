43d0a02fe95d5fe944e947ae84a0f26655301d89
Official3173 <42709932+Official3173@users.noreply.github.com>
2018-10-05 22:52:48 +1000

    Added dropdown functionality for Star Rating input

--- mywebsite/images/forms.py
+++ mywebsite/images/forms.py
@@ -1,9 +1,22 @@
 
 from django import forms
 
+IMAGE_CHOICES = [
+    ('0.5', '0.5'),
+    ('1', '1'),
+    ('1.5', '1.5'),
+    ('2', '2'),
+    ('2.5', '2.5'),
+    ('3', '3'),
+    ('3.5', '3.5'),
+    ('4', '4'),
+    ('4.5', '4.5'),
+    ('5', '5'),            
+]
+
 class ContactForm(forms.Form):
-    star_rating = forms.CharField()
-    kwh = forms.CharField()
-    model_num = forms.CharField()
+    star_rating = forms.CharField(label='Star Rating', widget=forms.Select(choices=IMAGE_CHOICES))
+    kwh = forms.CharField(label='kWh')
+    model_num = forms.CharField(label='Model Number')
     
     
